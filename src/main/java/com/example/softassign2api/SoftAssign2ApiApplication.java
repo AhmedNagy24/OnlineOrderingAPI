b22e83504d336e794c48115ca1f21861329058fe
Ahmed Nagy <anagy464@gmail.com>
2023-12-26 23:14:27 +0200

    Initial commit

---
+++ src/main/java/com/example/softassign2api/SoftAssign2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.softassign2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftAssign2ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftAssign2ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/softassign2api/SoftAssign2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softassign2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftAssign2ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
